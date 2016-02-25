2ecf81d5a402b53439175aa7d0ffedc1c409040e
Stephen Monroe <stephen.monroe@me.com>
2016-02-25 14:41:27 -0600

    Added files via upload

---
+++ SpinklrAudio.Bookmark.js
@@ -0,0 +1,9 @@
+javascript:(
+	function() {
+		var js = document.createElement('script');
+		js.setAttribute('type', 'text/javascript');
+		js.setAttribute('id', 'sprinklrAudioScript');
+		js.setAttribute('src', 'https://cdn.rawgit.com/rendermind/SprinklrAudio/master/sprinklr_audio.js');
+		document.head.appendChild(js);
+	}()
+);
